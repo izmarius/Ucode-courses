5711edeed6806dbd2ce3d6e8b2717daab2cffe28
izmarius <izmarius.im@gmail.com>
2020-07-12 12:50:36 +0300

    template

---
+++ Work/Javascript/template/template/munca.js
@@ -0,0 +1,30 @@
+// += operator
+//debugger;
+
+/*let str = 'hello world';
+let res = str.charAt(3);
+let res2 = str.charCodeAt(0);
+
+let str1 = 'wello world';
+let res3 = str1.endsWith('d');
+
+console.log(res);
+console.log(res2);
+console.log(res3);
+
+let res4 = String.fromCharCode(65);
+console.log(res4);
+
+let str4 = 'hello world, welcome to the universe.';
+let res5 = str4.indexOf('welcome');
+console.log(res5);
+
+let str5 = 'hello world';
+let res6 = str5.slice(1,3);
+console.log(res6);
+
+let str6 = 'ana are mere';
+let res7 = str6.split('?');
+console.log(res7);*/
+
+
