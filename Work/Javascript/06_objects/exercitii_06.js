95a487c12e1f6baa1a9f04896ba52599714ff41b
izmarius <izmarius.im@gmail.com>
2020-07-20 21:21:50 +0300

    exercitii_06-part2

--- Work/Javascript/06_objects/exercitii_06.js
+++ Work/Javascript/06_objects/exercitii_06.js
@@ -1,3 +1,15 @@
+const myCustomFilterFunction = function (callback) {
+    for (let i = 0; i < this.length; i++)
+        if (callback(this[i])) {
+            let aux = this[i];
+            this[i] = this[this.length - 1];
+            this[this.length - 1] = aux;
+            this.length -= 1;
+        }
+    return this;
+}
+Array.prototype.myfilter = myCustomFilterFunction;
+
 // 1
 const myCustomMapFunction = function (callback) {
     for (let i = 0; i < this.length; i++)
@@ -26,6 +38,10 @@ function Shelf (color, lightIntesity, type, allowProductNumbers) {
         return this.products;
     }
 
+    this.getAllowedProductNumbers = function () {
+        return this.allowProductNumbers;
+    }
+
     this.setColor = function (newColor) {
         this.color = newColor;
     }
@@ -47,6 +63,10 @@ function Product (description, cost, profit, shelfType) {
         return this.type;
     }
 
+    this.getCode = function () {
+        return this.code;
+    }
+
     this.getPrice = function () {
         return this.price;
     }
@@ -54,10 +74,13 @@ function Product (description, cost, profit, shelfType) {
 
 function Store () {
     var shelfs = [s1, s2, s3, s4, s5];
+    var soldProducts = [];
 
     this.receiveProduct = function (product) {
         shelfs.forEach(el => {
             if (el.getType() !== product.getType())
+                return false;
+            if (el.getProducts().length >= el.getAllowedProductNumbers())
                 return false;
             el.getProducts().push(product);
         })
@@ -67,8 +90,29 @@ function Store () {
         return shelfs.reduce(((acc, el) => acc += el.getProducts().reduce(((acc, el) => acc += el.getPrice()), 0)), 0);
     }
 
+    this.getSumPerEachCategory = function () {
+        return shelfs.map(el => el.getProducts().reduce(((acc, el) => acc += el.getPrice()), 0));
+    }
+
+    this.sellProduct = function (productCode) {
+        shelfs.forEach(el => el.getProducts().myfilter(el => {
+            if (el.getCode() !== productCode)
+                return false;
+            soldProducts.push(el);
+            return true;
+        }));
+    }
+
+    this.addShelf = function (newShelf) {
+        shelfs.push(newShelf);
+    }
+
     this.getShelfs = function () {
         return shelfs;
+    }
+
+    this.getSoldProducts = function () {
+        return soldProducts;
     }
 }
 
@@ -79,16 +123,27 @@ let s4 = new Shelf('orange', 60, 'clothes', 40);
 let s5 = new Shelf('white', 100, 'presentation', 2);
 
 let basketball = new Product('basketball', 10, 5, 'sport');
+let boxinggloves = new Product('boxingGloves', 10, 4, 'sport');
 let stuffedBear = new Product('stuffedBear', 8, 3, 'toys');
 let ring = new Product('ring', 50, 13, 'jewelry');
 let hoodie = new Product('hoodie', 15, 7, 'clothes');
 let bicycle = new Product('bicycle', 0, 0, 'presentation');
+let car = new Product('car', 0, 0, 'presentation');
 
 let store1 = new Store();
 store1.receiveProduct(basketball);
+store1.receiveProduct(boxinggloves);
 store1.receiveProduct(stuffedBear);
 store1.receiveProduct(ring);
 store1.receiveProduct(hoodie);
 store1.receiveProduct(bicycle);
+store1.receiveProduct(car);
+
 console.log(store1.getShelfs().map(el => el.getProducts()));
 console.log(store1.getSumOfAllProducts());
+console.log(store1.getSumPerEachCategory());
+
+// sell a prodduct, I chose one exsiting code because they are generated random
+store1.sellProduct(store1.getShelfs()[0].getProducts()[0].getCode());
+console.log(store1.getSoldProducts());
+console.log(store1.getShelfs().map(el => el.getProducts()));
