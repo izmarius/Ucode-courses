af194073c17d21718892f4413b6a9352975842b0
izmarius <izmarius.im@gmail.com>
2020-09-14 14:55:22 +0300

    added-num=1-and-changed-prevMax

--- Work/Javascript/12/exercitii_12.js
+++ Work/Javascript/12/exercitii_12.js
@@ -2,6 +2,10 @@
 
 const getCustomSequence = (num) => {
     let sequence = [];
+    if (num === 1) {
+        sequence.push(1);
+        num = num * 3 + 1;
+    }
     while (num !== 1) {
         sequence.push(num);
         num % 2 === 1 & num > 1 ? num = num * 3 + 1 : num = Math.floor(num / 2);
@@ -10,11 +14,14 @@ const getCustomSequence = (num) => {
     return sequence;
 };
 
+console.log(getCustomSequence(1));
+console.log(getCustomSequence(2));
+
 const getMaxSequenceLengthInInterval = (i, j) => {
     let maxSeq = 0;
     let currentMax = 0;
     for (let num = i; num <= j; num++) {
-        let previousMax  = currentMax;
+        let previousMax  = maxSeq;
         let currentSequence = getCustomSequence(num);
         currentMax = currentSequence.length;
         maxSeq = Math.max(previousMax, currentMax);
@@ -22,4 +29,4 @@ const getMaxSequenceLengthInInterval = (i, j) => {
     return maxSeq;
 };
 
-console.log(getMaxSequenceLengthInInterval(1,3));
+console.log(getMaxSequenceLengthInInterval(1,2));
