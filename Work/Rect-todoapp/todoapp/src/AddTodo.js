6411ba45c063a253399fdab6796f3f453acc9aac
izmarius <izmarius.im@gmail.com>
2020-09-23 22:33:10 +0300

    todoapp-update

--- Work/Rect-todoapp/todoapp/src/AddTodo.js
+++ Work/Rect-todoapp/todoapp/src/AddTodo.js
@@ -4,16 +4,20 @@ class AddTodo extends Component {
     state = {
         content: ''
     }
+    todoPattern = /^[a-zA-Z]{1,100}$/;
 
     handleChange = (e) => {
         this.setState({
             content: e.target.value.trim()
-        })
+        });
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.addTodo(this.state);
+        if (this.todoPattern.test(this.state.content)) {
+            this.props.addTodo(this.state);
+            e.target.reset();
+        }
         e.target.reset();
     }
 
--- Work/Rect-todoapp/todoapp/src/Todos.js
+++ Work/Rect-todoapp/todoapp/src/Todos.js
@@ -3,14 +3,23 @@ import React from "react";
 const Todos = (props) => {
   const { todos, deleteTodo } = props;
 
-
   const todoList = todos.length
     ? todos.map((todo) => {
         return (
-          <div className="collection-item" key={todo.id}>
-            <span onClick={(e) => {
-                deleteTodo(todo.id)
-            }}>{todo.content}</span>
+          <div className="collection-item row" key={todo.id}>
+            <div className="col s11" onClick={(e) => {
+              e.target.style.textDecoration="line-through";
+              e.target.nextSibling.classList.remove("hide");
+            }}>{todo.content}</div> 
+            <div className="col s1 center-align hide">
+              <span
+                onClick={(e) => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                X
+              </span>
+            </div>
           </div>
         );
       })
