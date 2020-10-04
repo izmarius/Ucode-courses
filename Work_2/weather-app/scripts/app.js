4c34a8bb81d6ad8c7858c3eba74abaa3dffc47ec
izmarius <izmarius.im@gmail.com>
2020-10-04 19:35:24 +0300

    update-ui

--- Work_2/weather-app/scripts/app.js
+++ Work_2/weather-app/scripts/app.js
@@ -1,8 +1,47 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector(".time");
+const icon = document.querySelector(".icon img");
+
+const getTimeImageSrc = (weather) => {
+  if (weather.IsDayTime) {
+    return 'img/day.svg';
+  }
+  return 'img/night.svg';
+};
+
+const getIconSrc = (weather) => {
+  return `img/icons/${weather.WeatherIcon}.svg`;
+};
+
+const updateUI = (data) => {
+  const cityDetails = data.cityDetails;
+  const cityWeather = data.weather;
+  console.log(cityWeather);
+
+  //update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${cityWeather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${cityWeather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    //remove d-none class from card
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+
+    //update night / day and icon images
+    time.setAttribute('src', getTimeImageSrc(cityWeather));
+    icon.setAttribute('src', getIconSrc(cityWeather));
+};
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
-  console.log(cityDetails);
   const weather = await getWeather(cityDetails.Key);
 
   return {
@@ -18,6 +57,10 @@ cityForm.addEventListener("submit", (e) => {
   cityForm.reset();
 
   updateCity(city)
-    .then((data) => {console.log(data);})
-    .catch((err) => {console.log(err);});
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
